7fdf981909b5e165b3055df98bf9badf302e6e0c
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-03 09:38:47 +0000

    changes made in datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "rohit@gmial.accountEmail"
+var accountPassword = "2222"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 // this is not allowed
+
+/*
+
+prefere not to use var because issue in block scope and functional scope
+
+*/
+
+accountEmail = "rk@gmail.com "
+accountPassword = "22"
+accountCity = "Bagru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState]);
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all java script code as newer version
+
+alert("3+3") // we are using node.js, not browser
+
+alert("3+3"
+    
+).  // code readablity should be high
+
+console.log("rohit");
+
+let name = "rohit" 
+let age = 20 
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => use double quotes
+// boolen => true or false
+// null => stand alone value
+// undefiened => 
+// symbol => unique 
+
+// object =>
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
+
+
