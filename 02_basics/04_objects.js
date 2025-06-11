6cdd93e6b313667c65ed3c624a86a5199e6c6189
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-11 09:28:41 +0000

    json talk and objects and API's

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,30 +1,30 @@
 // const appUser = Object()
 
-const appUser = {}
+// const appUser = {}
+// // console.log(appUser);
+
+// appUser.id = "123abc"
+// appUser.name = "rohit"
+// appUser.isLoggedIn = "false"
+
 // console.log(appUser);
 
-appUser.id = "123abc"
-appUser.name = "rohit"
-appUser.isLoggedIn = "false"
+// const regularUser ={
+//     email: "rohit@gmail.com",
+//     fullname:{
+//         userFullname:{
+//             fullname:"rohit",
+//             lastname:"kumawat"
+//         }
+//     }
 
-// console.log(appUser);
-
-const regularUser ={
-    email: "rohit@gmail.com",
-    fullname:{
-        userFullname:{
-            fullname:"rohit",
-            lastname:"kumawat"
-        }
-    }
-
-}
+// }
 
 // console.log(regularUser.fullname.userFullname.fullname);
 
-const  obj1 = {1:"a", 2:"b"}
-const  obj2 = {3:"a", 4:"b"}
-const  obj4 = {5:"a", 6:"b"}
+// const  obj1 = {1:"a", 2:"b"}
+// const  obj2 = {3:"a", 4:"b"}
+// const  obj4 = {5:"a", 6:"b"}
 
 
 // const obj3 = {obj1,obj2};
@@ -32,36 +32,57 @@ const  obj4 = {5:"a", 6:"b"}
 // const obj3 = Object.assign(obj1,obj2)
 // const obj3 = Object.assign({},obj1,obj2,obj4) // by using {} is done for the like it is used as target and the source is assigned to it. if we did not use this the outcome will be the same hence it is used for the best practices.
 
-const obj3 = {...obj1, ...obj2, ...obj4}
+// const obj3 = {...obj1, ...obj2, ...obj4}
 
-// console.log(obj3);
+// // console.log(obj3);
 
-const users = [
-    {
-        id:1,
-        email:"r@gmail.com",
+// const users = [
+//     {
+//         id:1,
+//         email:"r@gmail.com",
 
-    },
-    {
-        id:1,
-        email:"r@gmail.com",
+//     },
+//     {
+//         id:1,
+//         email:"r@gmail.com",
         
-    },
-    {
-        id:1,
-        email:"r@gmail.com",
+//     },
+//     {
+//         id:1,
+//         email:"r@gmail.com",
         
-    }
-]
+//     }
+// ]
 
-users[1].email
+// users[1].email
 
-console.log(appUser);
+// console.log(appUser);
 
-console.log(Object.keys(appUser));
-console.log(Object.values(appUser));
-console.log(Object.entries(appUser));
+// console.log(Object.keys(appUser));
+// console.log(Object.values(appUser));
+// console.log(Object.entries(appUser));
 
-console.log(appUser.hasOwnProperty('isLoggedIn'));
+// console.log(appUser.hasOwnProperty('isLoggedIn'));
 
 
+//  last lecture of the objects 
+
+const course = {
+    coursename:"Js in hindi",
+    price:"999",
+    courseInstructor:"Rohit"
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor:Instructor} = course
+console.log(Instructor);
+
+
+// {
+//     name:"rohit kumawat",
+//     coursename:"js in hindi",
+//     price:"free"
+
+// }
