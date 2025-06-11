b63666310a0b3f931dfeee7d03f92ac15d1be748
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-11 10:52:54 +0000

    functions with objects and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,7 +23,7 @@ function addTwoNumbers(number1,number2){
 
  const result = addTwoNumbers(3,5)
 
- console.log("Result: ",result);
+//  console.log("Result: ",result);
 
 
  
@@ -39,4 +39,39 @@ function loginUserMessage (username){
 }
 // console.log(loginUserMessage("rohit"))
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// next videos starts from down here
+
+function calculateCartPrice (val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "rohit",
+    price: 199
+}
+
+function handelObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+
+}
+// handelObject(user)
+
+handelObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
+
