ff77eefb4c76e94872b48a39667a2283dae73084
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-12 10:44:25 +0000

    scope and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,11 +6,59 @@ if(true){
     let a = 10
     const b = 20 
     // var c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+// =========================+++++++++++++++++++++++++++++++++++++========================+++++++++=
+
+function one (){
+    const username = "rohit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+if (true){
+    const username = "rohit"
+    
+    if (username === "rohit"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++++++intresting++++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+
+}
+
+console.log(addTwo(5))
+
+const addTwo = function(num){
+    return num + 2
+
+}
