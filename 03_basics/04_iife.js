370d6cd70afc64039513d04cc035c581d1b08337
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-12 12:33:26 +0000

    iife done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Fucntion Expressions (IIFE)
+
+(function chai (){
+    // named IIFI
+    console.log(`DB CONNECTED`);
+    
+}) ();
+
+(() => {
+    console.log(`DB CONNECTED TWO`);
+    
+
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+
+})('rohit')
