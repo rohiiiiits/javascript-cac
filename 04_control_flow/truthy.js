b901ca6b352691cd4fe14cfade59a83d8c57e111
rohiiiiits <r.rohitkumawat0@gmail.com>
2025-06-13 07:24:11 +0000

    switch and other control_flow done

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,52 @@
+// const userEmail = "r@gmail.com"
+
+// if (userEmail) {
+//     console.log("got user email");
+    
+// } else {
+//     console.log("don't have user email");
+    
+// }
+
+// falsy values
+
+// false, 0 , -0 , BigInt 0n , "" , null , undefined , NaN 
+
+// truthy values
+
+// "0" , 'false' , " " , [] , {} , function(){} 
+
+// if ( userEmail.length === 0){
+//     console.log("array is empty"); 
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length) {
+//     console.log("object is empty");
+    
+// }
+
+
+// nullish coalescing Operator (??) : null and undefined
+
+// let vel1;
+// // vel1 = 5 ?? 10
+// // vel1 = null ?? 10
+
+// // vel1 = undefined ?? 15
+
+// vel1 = null ?? 10 ?? 15
+
+
+// console.log(vel1);
+
+
+// Terniary Operator 
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
+
