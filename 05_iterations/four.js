c4d953ffdf57d7a0c7f5623379bb596b6e6a5c01
Rohit Kumawat <r.rohitkumawat0@gmail.com>
2025-06-17 12:56:54 +0000

    for while

---
+++ 05_iterations/four.js
@@ -0,0 +1,13 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// for in loop used for objects
+
+for (const key in myObject) {
+   console.log(key);
+   
+}
