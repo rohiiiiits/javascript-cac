81a98fe462b9369f421b8ecce9b53068b30fe4dd
Rohit Kumawat <r.rohitkumawat0@gmail.com>
2025-06-17 09:17:29 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,43 @@
 // for
 
-for (let index = 0; index < 10; index++) {
-    const element =index;
-    console.log(element);   
+for (let i = 0; i <= 10; i++) {
+    const element =i;
+    if (element === 5) {
+        // console.log("5 is the best number");
+    }
+    // console.log(element);   
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`outer loop value:${i}`);
+    
+   for (let j = 0; j <= 10; j++) {
+    // console.log(`inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*' + j + ' = ' + i*j);
+    
+   }
+    
+}
+
+let myArry = ["flash" , "batman" , "superman"]
+// console.log(myArry.length);
+
+for (let index = 0; index < myArry.length; index++) {
+    const element = myArry[index];
+    // console.log(element);  
+}
+
+
+//  break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+       break
+        
+        
+    }
+    console.log(index);
+    
+    
 }
