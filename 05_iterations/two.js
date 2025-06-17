a57f051ce04408a93ab16af9bf32bd8d33db8242
Rohit Kumawat <r.rohitkumawat0@gmail.com>
2025-06-17 09:32:42 +0000

    while completed

---
+++ 05_iterations/two.js
@@ -0,0 +1,30 @@
+// while
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+    
+    
+// }
+
+// let myArry = ["flash", "batman" , "superman"]
+
+// let arr = 0
+// while (arr < myArry.length) {
+//     console.log(`value is ${myArry[arr]}`);
+//    arr = arr + 1
+    
+// }
+
+
+// do while loop
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
