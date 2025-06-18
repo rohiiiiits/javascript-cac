81a147f78529795eff2fb3676748ae36809fc939
Rohit Kumawat <r.rohitkumawat0@gmail.com>
2025-06-18 08:49:12 +0000

    for in loops

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -8,6 +8,22 @@ const myObject = {
 // for in loop used for objects
 
 for (const key in myObject) {
-   console.log(key);
+// console.log(`${key} shortcut is for ${myObject[key]}`);
    
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map ()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+//     // console.log(key);
+        
+//     }
